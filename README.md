dde72741b1844e6f2ff80f4756df23b381f40d4d
gustavojorge <gustavojorge080@gmail.com>
2025-02-01 22:51:17 -0300

    first

---
+++ README.md
@@ -0,0 +1,15 @@
+# Atividade referente a Sprint4 - "Aplicando boas práticas".
+
+Segue a atividade abaixo do projeto de Todo List que foi feita durante as aulas. Agora, **refatorado e com efeito de 'concluído'**.
+
+- Primeira Versão:
+
+![img_2.png](img_2.png)
+
+- Segunda versão:
+
+![img_1.png](img_1.png)
+
+- Terceira versão:
+
+![img_6.png](img_6.png)
---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_flutter/components/task.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool opacity = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: Text(
+          "Tarefas",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: AnimatedOpacity(
+        opacity: opacity ? 1 : 0,
+        duration: Duration(milliseconds: 800),
+        child: ListView(
+          children: [
+            Task(
+                "Aprender flutter",
+                "assets/images/dash.png",
+                4),
+            Task(
+                "Andar de bike",
+                "assets/images/bike.webp",
+                2),
+            Task(
+                "Ler",
+                "assets/images/ler.jpg",
+                1),
+            Task(
+                "Meditar",
+                "assets/images/meditar.jpeg",
+                5),
+            Task("Jogar",
+                "assets/images/play.jpg", 1),
+            SizedBox(
+              height: 80,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            opacity = !opacity;
+          });
+        },
+        backgroundColor: Colors.blue,
+        child: opacity
+            ? Icon(Icons.remove_red_eye, color: Colors.white)
+            : Icon(Icons.remove_red_eye, color: Colors.green),
+      ),
+    );
+  }
+}
